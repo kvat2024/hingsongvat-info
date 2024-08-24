e2ef191431ca6f46c223f8d7b9a705e1952390d9
HING_SONGVAT <kvattic6@gmail.com>
2024-08-24 22:40:29 +0700

    initail commit

--- javascript/index.js
+++ javascript/index.js
@@ -3,3 +3,20 @@ hamburger.onclick = function () {
   navBar = document.querySelector(".nav-bar");
   navBar.classList.toggle("active");
 };
+
+ 
+const favoriteColors = ["#3a86ff", "#00b4d8", "#00b4d8", "#ff0000"]; 
+
+function getRandomFavoriteColor() {
+    return favoriteColors[Math.floor(Math.random() * favoriteColors.length)];
+}
+
+function changeTextColor() {
+    let randomColor = getRandomFavoriteColor();
+    let spans = document.querySelectorAll('.title-me span');
+    spans.forEach(span => {
+        span.style.color = randomColor;
+    });
+}
+
+setInterval(changeTextColor, 100); 
