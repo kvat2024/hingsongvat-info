ecfaf86942b5311935e615dfda66754c243a1c7b
HING_SONGVAT <kvattic6@gmail.com>
2024-08-25 07:51:06 +0700

    initail commit

--- javascript/index.js
+++ javascript/index.js
@@ -1,22 +1,31 @@
+document.addEventListener(
+  "touchstart",
+  function (event) {
+    if (event.touches.length > 1) {
+      event.preventDefault();
+    }
+  },
+  { passive: false }
+);
+
 hamburger = document.querySelector(".hamburger");
 hamburger.onclick = function () {
   navBar = document.querySelector(".nav-bar");
   navBar.classList.toggle("active");
 };
 
- 
-const favoriteColors = ["#3a86ff", "#00b4d8", "#00b4d8", "#ff0000"]; 
+const favoriteColors = ["#3a86ff", "#00b4d8", "#00b4d8", "#ff0000"];
 
 function getRandomFavoriteColor() {
-    return favoriteColors[Math.floor(Math.random() * favoriteColors.length)];
+  return favoriteColors[Math.floor(Math.random() * favoriteColors.length)];
 }
 
 function changeTextColor() {
-    let randomColor = getRandomFavoriteColor();
-    let spans = document.querySelectorAll('.title-me span');
-    spans.forEach(span => {
-        span.style.color = randomColor;
-    });
+  let randomColor = getRandomFavoriteColor();
+  let spans = document.querySelectorAll(".title-me span");
+  spans.forEach((span) => {
+    span.style.color = randomColor;
+  });
 }
 
-setInterval(changeTextColor, 100); 
+setInterval(changeTextColor, 100);
